3ff7042597947449e7537da4a8dfba16a642584f
Lorena Kauane <lorena_kauani@hotmail.com>
2021-08-24 22:05:30 -0300

    Add Style hover menu header item

--- components/Header/HeaderItem/styles.js
+++ components/Header/HeaderItem/styles.js
@@ -15,5 +15,19 @@ export const Item = styled.a`
     margin-left: 8px;
     letter-spacing: 1.42px;
     text-transform: uppercase;
+
+    --border-color: rgb(249, 249, 249);
+    --border-width: 2px;
+    --bottom-distance: -1px;
+
+    background-image: linear-gradient(var(--border-color), var(--border-color));
+    background-size: 0% var(--border-width);
+    background-repeat: no-repeat;
+    transition: background-size 0.3s;
+    background-position: 0 calc(100% - var(--bottom-distance));
+
+    :hover {
+      background-size: 100% var(--border-width);
+    }
   }
 `
